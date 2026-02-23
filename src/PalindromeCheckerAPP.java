afeff8c56b61f5d99a5991ad43c26e6ad7e306fd
Pranav-0018 <gpranav741@gmail.com>
2026-02-23 09:17:17 +0530

    inital commit

---
+++ src/PalindromeCheckerAPP.java
@@ -0,0 +1,15 @@
+/*
+     * @Author Pranav
+     * @version 1.0
+     *  Use Case 1 :
+     * PalindromeCheckerApp
+     * Starting The System and Initialize the System
+     */
+    public class PalindromeCheckerAPP{
+        public static void main(String[] args){
+            System.out.println("Welcome to Palindrome Checker Mangement System");
+            System.out.println("Version : 1.0");
+            System.out.println("System Initialized Successfully.");
+        }
+    }
+
