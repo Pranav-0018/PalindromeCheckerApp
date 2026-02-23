3a6cfbdc4a317fcfe5fd300348a771469aa0ed7e
Pranav-0018 <gpranav741@gmail.com>
2026-02-23 09:44:14 +0530

    uc2 submitted

--- src/PalindromeCheckerAPP.java
+++ src/PalindromeCheckerAPP.java
@@ -10,6 +10,20 @@
             System.out.println("Welcome to Palindrome Checker Mangement System");
             System.out.println("Version : 1.0");
             System.out.println("System Initialized Successfully.");
+            String str = "madam";
+
+            // Reverse string
+            String rev = "";
+            for (int i = str.length() - 1; i >= 0; i--) {
+                rev = rev + str.charAt(i);
+            }
+
+            // Check palindrome
+            if (str.equals(rev)) {
+                System.out.println(str + " is a Palindrome");
+            } else {
+                System.out.println(str + " is not a Palindrome");
+            }
         }
     }
 
